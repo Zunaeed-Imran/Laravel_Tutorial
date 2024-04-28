ac64201913807535e1013739e6bd18b0ec3be9aa
imran <zunaeede@gmail.com>
2024-04-28 11:59:03 +0600

    main - make it for blade file.

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-  <h2><?php echo $listing['title'] ?></h2>
-  <h2><?php echo $listing['description'] ?></h2>
+  <h2>{{$listing['title']}}</h2>
+  <h2>{{$listing['description']}}</h2>
 
 @endsection
