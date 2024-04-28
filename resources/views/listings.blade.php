6a230fb9fbae73a80e767a3135f2d4f1a330cc74
imran <zunaeede@gmail.com>
2024-04-28 15:51:47 +0600

    practice1 - add lsitings in the practice branch.

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,3 +1,19 @@
+@extends('layout')
+
+@section('content')
+    
 
 
-{{}}
+<h1>Laravel 11 Learning</h1>
+
+<h2>{{$heading}}</h2>
+
+
+@foreach ($listings as $listing)
+  <h2>
+    <a href="/listings{{$listing['id']}}">{{$listing['title']}}</a>
+  </h2>
+  <h2>{{$listing['description']}}</h2>
+@endforeach
+
+@endsection
