d1e29a2111c449b78de4229161a1dc7121002f92
imran <zunaeede@gmail.com>
2024-05-02 13:48:19 +0600

    crud - installed the tailwind css in this crud project.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        {{-- Tailwind post CSS laravel --}}
+          @vite('resources/css/app.css')
+
         <title>Laravel</title>
 
         <!-- Fonts -->
