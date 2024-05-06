ede8cf112be0fc597c03fffbdb372a24b13dd851
imran <zunaeede@gmail.com>
2024-05-06 17:20:19 +0600

    crud - add delete option. in the list button.

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use app\Models\Listingitem;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListItem;
 
 class TodoListController extends Controller
 {
 
     public function index()
     {
-        return view('welcome', ['listingItems' => Listingitem::all()]);
+        return view('welcome', ['listingItems' => Listingitem::where('is_complate', 0)->get()]);
     }
 
     public function markComplete($id)
     {
-        \log($id);
+        $listItem = ListItem::find($id);
+        $listItem->is_complate = 1;
+        $listItem->save();
+
         return redirect('/');
     }
     public function saveItem(Request $request)
